ac8bda683d25eeba4aea080d9e78ebdd64297e17
rjhdby <andrewgrom@rambler.ru>
2018-01-11 17:58:26 +0300

    add some tests

--- test/WeightedListTest.java
+++ test/WeightedListTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -88,7 +90,7 @@ class WeightedListTest {
         array.add("a", 5);
         array.add("b", 1);
         array.add("c", 1);
-        assertArrayEquals(new String[]{"10", "a", "5", "4", "3", "2", "c", "b", "1"}, array.toArray());
+        assertArrayEquals(new String[]{"10", "5", "a", "4", "3", "2", "1", "b", "c"}, array.toArray());
     }
 
     @Test
@@ -171,30 +173,72 @@ class WeightedListTest {
 
     @Test
     void remove() {
+        WeightedList<String> array = filledList();
+        array.remove("4");
+        assertArrayEquals(new String[]{"5", "3", "2", "1"}, array.toArray());
     }
 
     @Test
     void containsAll() {
+        WeightedList<String> array   = filledList();
+        List<String>         compare = new ArrayList<>();
+        compare.add("1");
+        compare.add("2");
+        assertTrue(array.containsAll(compare));
+        compare.add("20");
+        assertFalse(array.containsAll(compare));
     }
 
     @Test
     void addAll() {
+        WeightedList<String> array = filledList();
+        List<String>         add   = new ArrayList<>();
+        add.add("10");
+        add.add("20");
+        array.addAll(add);
+        assertArrayEquals(new String[]{"5", "4", "3", "2", "1", "10", "20"}, array.toArray());
     }
 
     @Test
     void addAll1() {
+        WeightedList<String> array = filledList();
+        List<String>         add   = new ArrayList<>();
+        add.add("10");
+        add.add("20");
+        array.addAll(3, add);
+        assertArrayEquals(new String[]{"5", "4", "3", "2", "1", "10", "20"}, array.toArray());
     }
 
     @Test
     void removeAll() {
+        WeightedList<String> array  = filledList();
+        List<String>         remove = new ArrayList<>();
+        remove.add("4");
+        remove.add("2");
+        remove.add("20");
+        array.removeAll(remove);
+        assertArrayEquals(new String[]{"5", "3", "1"}, array.toArray());
+        assertNull(array.getWeight("2"));
     }
 
     @Test
     void retainAll() {
+        WeightedList<String> array  = filledList();
+        List<String>         retain = new ArrayList<>();
+        retain.add("4");
+        retain.add("2");
+        retain.add("20");
+        array.retainAll(retain);
+        assertArrayEquals(new String[]{"4", "2"}, array.toArray());
+        assertNull(array.getWeight("5"));
     }
 
     @Test
     void clear() {
+        WeightedList<String> array = filledList();
+        array.clear();
+        assertTrue(array.isEmpty());
+        assertNull(array.getWeight("5"));
     }
 
     @Test
@@ -207,6 +251,9 @@ class WeightedListTest {
 
     @Test
     void remove1() {
+        WeightedList<String> array = filledList();
+        assertEquals("4", array.remove(1));
+        assertArrayEquals(new String[]{"5", "3", "2", "1"}, array.toArray());
     }
 
     @Test
